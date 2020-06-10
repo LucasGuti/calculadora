17a4f8e79ccf9280dae455d76ad8ee38326b1eab
Lucas <lucasismaelgutierrez@gmail.com>
2020-06-09 21:56:33 -0300

    Se creo la funcion Init y las variables de cada tecla

---
+++ js/calculadora.js
@@ -0,0 +1,19 @@
+function init(){
+   let resultado = document.getElementById("resultado");
+   let suma = document.getElementById("sumar");
+   let resta = document.getElementById("restar");
+   let multiplicacion = document.getElementById("multiplicar");
+   let division = document.getElementById("dividir");
+   let borrar = document.getElementById("borrar");
+   let igual = document.getElementById("igual");
+   let uno = document.getElementById("uno");
+   let dos = document.getElementById("dos");
+   let tres = document.getElementById("tres");
+   let cuatro = document.getElementById("cuatro");
+   let cinco = document.getElementById("cinco");
+   let seis = document.getElementById("seis");
+   let siete = document.getElementById("siete");
+   let ocho = document.getElementById("ocho");
+   let nueve = document.getElementById("nueve");
+   let cero = document.getElementById("cero");
+}
