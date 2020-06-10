4dc63f3eccac331404cc57a177d60b3151908cf8
Lucas <lucasismaelgutierrez@gmail.com>
2020-06-09 22:40:45 -0300

    Se agregaron eventos a las teclas numericas

--- js/calculadora.js
+++ js/calculadora.js
@@ -16,4 +16,38 @@ function init(){
    let ocho = document.getElementById("ocho");
    let nueve = document.getElementById("nueve");
    let cero = document.getElementById("cero");
+
+   //Agregar eventos
+   uno.onclick = function(e){
+      resultado.textContent = resultado.textContent + "1";
+   }
+   dos.onclick = function(e){
+      resultado.textContent = resultado.textContent + "2";
+   }
+   tres.onclick = function(e){
+      resultado.textContent = resultado.textContent + "3";
+   }
+   cuatro.onclick = function(e){
+      resultado.textContent = resultado.textContent + "4";
+   }
+   cinco.onclick = function(e){
+      resultado.textContent = resultado.textContent + "5";
+   }
+   seis.onclick = function(e){
+      resultado.textContent = resultado.textContent + "6";
+   }
+   siete.onclick = function(e){
+      resultado.textContent = resultado.textContent + "7";
+   }
+   ocho.onclick = function(e){
+      resultado.textContent = resultado.textContent + "8";
+   }
+   nueve.onclick = function(e){
+      resultado.textContent = resultado.textContent + "9";
+   }
+   cero.onclick = function(e){
+      resultado.textContent = resultado.textContent + "0";
+   }
+
+
 }
